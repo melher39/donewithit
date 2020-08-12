2afdc5cbead56873d2210e17c7cda9db0749ce7c
Melvin Hernandez <melher39@gmail.com>
2020-08-11 22:47:19 -0500

    npm i @react-native-community/hooks and used a couple of APIs to retrieve responsive width and height and orientation mode info

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Alert, Button, StatusBar, Platform } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  SafeAreaView,
+  Alert,
+  Button,
+  StatusBar,
+  Platform,
+  View
+} from "react-native";
+import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
+  const { landscape } = useDeviceOrientation();
   return (
     <SafeAreaView style={styles.container}>
-      <Button
-        title="Click me"
-        onPress={() => Alert.prompt("My title", "My message", text => console.log(text))}
-      />
+      <View style={{
+        backgroundColor: "dodgerblue",
+        width: "100%",
+        height: landscape ? "100%" : "30%"
+      }}
+      >
+
+      </View>
     </SafeAreaView>
   );
 }
@@ -15,7 +30,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "orange",
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   }
 });
