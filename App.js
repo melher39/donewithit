538c61c486a165fec1f13d13c3557eb7a9dc2298
Melvin Hernandez <melher39@gmail.com>
2020-08-12 21:58:53 -0500

    practicing flex properties such as justifyContent and alignItems

--- App.js
+++ App.js
@@ -1,35 +1,42 @@
 import React from "react";
 import {
-  Dimensions,
-  StyleSheet,
-  SafeAreaView,
-  Alert,
-  Button,
-  StatusBar,
-  Platform,
   View
 } from "react-native";
-import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={{
-        backgroundColor: "dodgerblue",
-        width: "100%",
-        height: landscape ? "100%" : "30%"
-      }}
-      >
 
-      </View>
-    </SafeAreaView>
+  return (
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 300,
+          alignSelf: "flex-start"
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 200
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100
+        }}
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-  }
-});
