a3bc896eb81ae207327d692a6e54d7dabe70c792
Melvin Hernandez <melher39@gmail.com>
2020-08-13 22:45:50 -0500

    started the challenge. created new directoires for images and new components. successfully made the welcome screen

--- App.js
+++ App.js
@@ -2,43 +2,10 @@ import React from "react";
 import {
   View
 } from "react-native";
+import WelcomeScreen from "./screens/WelcomeScreen.js";
 
 export default function App() {
-
   return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center"
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 20,
-          position: "absolute"
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100
-        }}
-      />
-    </View>
+    <WelcomeScreen />
   );
 }
